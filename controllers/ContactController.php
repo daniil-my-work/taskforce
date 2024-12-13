b1669e33bacf993e668205708470a5799dd4eac2
Daniil-veles <d.suvorov@skveles.ru>
2024-12-13 10:30:36 +0300

    практика

--- controllers/ContactController.php
+++ controllers/ContactController.php
@@ -3,9 +3,28 @@
 namespace app\controllers;
 
 use app\models\Contact;
+use yii\db\Query;
 use yii\web\Controller;
 
 class ContactController extends Controller
 {
-    public function actionIndex() {}
+    public function actionIndex()
+    {
+        $query = new Query();
+
+        // $query->select(['name', 'label', 'phone'])->from('contact');
+        // $query->select(['name', 'label', 'phone'])->from('contact')->where(['id' => '10']);
+
+        $query->select(['name', 'email'])
+            ->from('user')
+            ->where(['position' => 'super', 'name' => 'Daniil'])
+            ->orderBy(['id' => SORT_ASC])
+            ->limit(20);
+
+        $contacts = $query->all();
+
+        foreach ($contacts as $contact) {
+            print($contact->name);
+        }
+    }
 }
