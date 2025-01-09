fd329a5711f1f297edfff2e78ad79da59a4b73ee
Daniil-veles <d.suvorov@skveles.ru>
2025-01-09 11:22:43 +0300

    способы отправки запроса из прилы

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use yii\web\Controller;
+use GuzzleHttp\Client;
 
 class SiteController extends Controller
 {
@@ -23,10 +24,68 @@ class SiteController extends Controller
 
     public function actionTest()
     {
-        $response = file_get_contents('https://randomuser.me/api/');
-        $data = json_decode($response, true);
-        var_dump($data['results']);
+        // Вариант 1
+        // // Инициализация cURL
+        // $ch = curl_init('https://randomuser.me/api/');
 
-        // return 'Тест';
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Возвращать результат в виде строки
+        // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Следовать за редиректами
+
+        // // Выполнение запроса
+        // $result = curl_exec($ch);
+
+        // // Проверка на ошибки
+        // if ($result === false) {
+        //     echo "Ошибка cURL: " . curl_error($ch);
+        // } else {
+        //     // Преобразуем JSON-ответ в массив
+        //     $data = json_decode($result, true);
+
+        //     // Выводим результат
+        //     echo "<pre>";
+        //     print_r($data);
+        //     echo "</pre>";
+        // }
+
+        // curl_close($ch);
+
+
+        // Вариант 2
+        $url = 'https://randomuser.me/api/';
+        $options = [
+            'http' => [
+                'method' => 'GET',
+                'header'  => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)\r\n",
+                'follow_location' => true,
+            ]
+        ];
+
+        // Создание контекста
+        $context = stream_context_create($options);
+
+        // Выполнение запроса и получение ответа
+        $result = file_get_contents($url, false, $context);
+
+        // Проверка на ошибки
+        if ($result === false) {
+            echo "Ошибка при выполнении запроса"; 
+        } else {
+            // Преобразуем JSON-ответ в массив
+            $data = json_decode($result, true);
+
+            // Выводим результат
+            echo "<pre>";
+            print_r($data);
+            echo "</pre>";
+        }
+
+
+        // Вариант 3
+        // $client = new Client([
+        //     'base_uri' => 'https://randomuser.me/api/',
+        // ]);
+        // $response = $client->get('');
+        // $data = json_decode($response->getBody(), true);
+        // var_dump($data['results']);
     }
 }
