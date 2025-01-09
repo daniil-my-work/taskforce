40fdb527f217b0ff2c6e5c121a43d002d472ab9c
Daniil-veles <d.suvorov@skveles.ru>
2025-01-09 12:08:41 +0300

    добавил отправку

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -27,13 +27,10 @@ class SiteController extends Controller
         // Вариант 1
         // // Инициализация cURL
         // $ch = curl_init('https://randomuser.me/api/');
-
         // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Возвращать результат в виде строки
         // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Следовать за редиректами
-
         // // Выполнение запроса
         // $result = curl_exec($ch);
-
         // // Проверка на ошибки
         // if ($result === false) {
         //     echo "Ошибка cURL: " . curl_error($ch);
@@ -46,46 +43,23 @@ class SiteController extends Controller
         //     print_r($data);
         //     echo "</pre>";
         // }
-
         // curl_close($ch);
 
+        // $client = new Client();
+        // try {
+        //     $response = $client->request('GET', 'https://randomuser.me/api/', [
+        //         'headers' => [
+        //             'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        //         ]
+        //     ]);
 
-        // Вариант 2
-        $url = 'https://randomuser.me/api/';
-        $options = [
-            'http' => [
-                'method' => 'GET',
-                'header'  => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)\r\n",
-                'follow_location' => true,
-            ]
-        ];
-
-        // Создание контекста
-        $context = stream_context_create($options);
-
-        // Выполнение запроса и получение ответа
-        $result = file_get_contents($url, false, $context);
-
-        // Проверка на ошибки
-        if ($result === false) {
-            echo "Ошибка при выполнении запроса"; 
-        } else {
-            // Преобразуем JSON-ответ в массив
-            $data = json_decode($result, true);
-
-            // Выводим результат
-            echo "<pre>";
-            print_r($data);
-            echo "</pre>";
-        }
-
-
-        // Вариант 3
-        // $client = new Client([
-        //     'base_uri' => 'https://randomuser.me/api/',
-        // ]);
-        // $response = $client->get('');
-        // $data = json_decode($response->getBody(), true);
-        // var_dump($data['results']);
+        //     $result = $response->getBody()->getContents();
+        //     $data = json_decode($result, true);
+        //     echo "<pre>";
+        //     print_r($data);
+        //     echo "</pre>";
+        // } catch (\GuzzleHttp\Exception\RequestException $e) {
+        //     echo "Ошибка запроса: " . $e->getMessage();
+        // }
     }
 }
